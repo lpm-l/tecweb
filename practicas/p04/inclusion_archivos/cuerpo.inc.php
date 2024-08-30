38132007312089ee65b93c958a4db92fa46333e5
Luisp <luispala96@gmail.com>
2024-08-29 18:12:18 -0600

    Se cambiaron algunas partes erróneas en el código de php

--- practicas/p04/inclusion_archivos/cuerpo.inc.php
+++ practicas/p04/inclusion_archivos/cuerpo.inc.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1> Este es el cuerpo del documento </h1>";
-echo "<h2> Este es el cuerpo del documento </h2>";
+    echo "<h1> Este es el cuerpo del documento </h1>";
+    echo "<h2> Este es el cuerpo del documento </h2>";
 ?>
--- practicas/p04/inclusion_archivos/encabezado.inc.php
+++ practicas/p04/inclusion_archivos/encabezado.inc.php
@@ -2,7 +2,7 @@
 <?php
     $variable1="PHP 5";
 ?>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang=“es">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <?php
@@ -11,10 +11,9 @@
     </head>
     <body>
         <?php 
-            $variableext=« Este texto proviene del archivo incluido";
+            $variableext=" Este texto proviene del archivo incluido";
             echo "<div><h1 style=\"border-width:5;border-style:double;background-color:#ffcc99;\">
             Bienvenido en el sitio $variable1 </h1>";
             echo "<h3> $variableext</h3>";
             echo "Nombre de archivo ejecutado: ", $_SERVER['PHP_SELF'],"&nbsp;&nbsp;&nbsp;";
-            echo " Nombre del archivo incluido : ", __FILE__ ,"</div> "; ← (3)
-        ?>
+            echo " Nombre del archivo incluido : ", __FILE__ ,"</div> ";
