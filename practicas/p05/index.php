4455fc64759b93b3c3b9a8fc4837240b40dc95b5
Luisp <luispala96@gmail.com>
2024-09-03 17:28:06 -0600

    Se agrego la respuesta a la pregunta 1

---
+++ practicas/p05/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>P05</title>
+</head>
+<body>
+    <?php
+    echo "<h2>Pregunta 2</h2>";
+    echo "<p>".'Variables válidas: $_myvar, $_7var, $myvar, $var7, $_element1'."<br>";
+    echo 'Estas variables son válidas ya que todas empiezan con letras o guion bajo'."<br>";
+    echo 'Variables inválidas: myvar, $house*5'."<br>";
+    echo "Estas variables no son válidas ya que tiene que iniciar con $, y no puede tener carácteres tales como * o /</p>"
+
+
+
+    ?>
+</body>
+</html>
