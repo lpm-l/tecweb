b33510461d86c13ea570bfb4d1e9a2156112b97a
Luisp <luispala96@gmail.com>
2024-09-03 20:11:47 -0600

    Se agrego la respuesta a la pregunta 2

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -6,13 +6,24 @@
 </head>
 <body>
     <?php
-    echo "<h2>Pregunta 2</h2>";
+    echo "<h2>Pregunta 1</h2>";
     echo "<p>".'Variables válidas: $_myvar, $_7var, $myvar, $var7, $_element1'."<br>";
     echo 'Estas variables son válidas ya que todas empiezan con letras o guion bajo'."<br>";
     echo 'Variables inválidas: myvar, $house*5'."<br>";
     echo "Estas variables no son válidas ya que tiene que iniciar con $, y no puede tener carácteres tales como * o /</p>"
 
+    echo "<br><h2>Pregunta 2</h2>";
 
+    #asignación de variables
+
+    $a = "ManejadorSQL";
+    $b = 'MySQL';
+    $c = &$a;
+
+    echo "Los valores de las variables son:<br>";
+    echo "a = $a";
+    echo "b = $b";
+    echo "c = $c";
 
     ?>
 </body>
