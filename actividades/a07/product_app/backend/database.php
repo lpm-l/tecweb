1bbbd82e7781db3df4f3893d348ee1dc3a47c5c6
Luisp <luispala96@gmail.com>
2024-11-05 14:18:32 -0600

    inicio actividad 7

---
+++ actividades/a07/product_app/backend/database.php
@@ -0,0 +1,16 @@
+<?php
+    $conexion = @mysqli_connect(
+        'localhost',
+        'root',
+        'Z#~W2xwamQ69jQq',
+        'marketzone'
+    );
+
+
+    /**
+     * NOTA: si la conexión falló $conexion contendrá false
+     **/
+    if(!$conexion) {
+        die('¡Base de datos NO conextada!');
+    }
+?>
