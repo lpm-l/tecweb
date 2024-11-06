32a9aeaa18d0a1f0c0fdbd756d4789cefeaf0d55
Luisp <luispala96@gmail.com>
2024-11-05 20:09:47 -0600

    funciones agregadas

--- actividades/a07/product_app/backend/myapi/Products.php
+++ actividades/a07/product_app/backend/myapi/Products.php
@@ -1,11 +1,189 @@
 <?php
     class Products extends DataBase{
-        public $response;
+        public $data;
         public function __construct($user = "root",$pass="Z#~W2xwamQ69jQq", $db="marketzone"){
             parent::__construct($user, $pass, $db);
 
+            $this->data = "Nada";
 
         }
+
+        public function add(){
+            // SE OBTIENE LA INFORMACIÓN DEL PRODUCTO ENVIADA POR EL CLIENTE
+            $producto = file_get_contents('php://input');
+            $data = array(
+                'status'  => 'error',
+                'message' => 'Ya existe un producto con ese nombre'
+            );
+            if(!empty($producto)) {
+                // SE TRANSFORMA EL STRING DEL JASON A OBJETO
+                $jsonOBJ = json_decode($producto);
+                // SE ASUME QUE LOS DATOS YA FUERON VALIDADOS ANTES DE ENVIARSE
+                $sql = "SELECT * FROM productos WHERE nombre = '{$jsonOBJ->nombre}' AND eliminado = 0";
+                $result = $conexion->query($sql);
+                
+                if ($result->num_rows == 0) {
+                    $conexion->set_charset("utf8");
+                    $sql = "INSERT INTO productos VALUES (null, '{$jsonOBJ->nombre}', '{$jsonOBJ->marca}', '{$jsonOBJ->modelo}', {$jsonOBJ->precio}, '{$jsonOBJ->detalles}', {$jsonOBJ->unidades}, '{$jsonOBJ->imagen}', 0)";
+                    if($conexion->query($sql)){
+                        $data['status'] =  "success";
+                        $data['message'] =  "Producto agregado";
+                    } else {
+                        $data['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($conexion);
+                    }
+                }
+
+                $result->free();
+                // Cierra la conexion
+                $conexion->close();
+            }
+        }
+
+        public function delete(){
+                // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+            $data = array( );
+            // SE VERIFICA HABER RECIBIDO EL ID
+            if( isset($_POST['id']) ) {
+                $id = $_POST['id'];
+                // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+                $sql = "UPDATE productos SET eliminado=1 WHERE id = {$id}";
+                if ( $conexion->query($sql) ) {
+                    $data['status'] =  "success";
+                    $data['message'] =  "Producto eliminado";
+                } else {
+                    $data['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($conexion);
+                }
+                $conexion->close();
+            } 
+        }
+
+        public function edit(){
+            $producto = file_get_contents('php://input');
+            $data = array(
+                'status'  => 'error',
+                'message' => 'Ya existe un producto con ese nombre');
+        
+            if(!empty($producto)) {
+                $jsonOBJ = json_decode($producto);
+        
+                $sql = "UPDATE productos SET nombre = '{$jsonOBJ->nombre}', marca = '{$jsonOBJ->marca}', modelo = '{$jsonOBJ->modelo}', precio = {$jsonOBJ->precio}, detalles = '{$jsonOBJ->detalles}', unidades = {$jsonOBJ->unidades}, imagen = '{$jsonOBJ->imagen}' WHERE id = $jsonOBJ->id";
+                $result =  $conexion->query($sql);
+        
+                if($conexion->query($sql)){
+                    $data['status'] =  "success";
+                    $data['message'] =  "Producto modificado";
+                } else {
+                    $data['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($conexion);
+                }
+                // Cierra la conexion
+                $conexion->close();
+        
+            }
+        }
+
+        public function list(){
+            // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+            $data = array();
+
+            // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+            if ( $result = $conexion->query("SELECT * FROM productos WHERE eliminado = 0") ) {
+                // SE OBTIENEN LOS RESULTADOS
+                $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+                if(!is_null($rows)) {
+                    // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                    foreach($rows as $num => $row) {
+                        foreach($row as $key => $value) {
+                            $data[$num][$key] = utf8_encode($value);
+                        }
+                    }
+                }
+                $result->free();
+            } else {
+                die('Query Error: '.mysqli_error($conexion));
+            }
+            $conexion->close();
+        }
+
+        public function search(){
+            // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+            $data = array();
+            // SE VERIFICA HABER RECIBIDO EL ID
+            if( isset($_POST['search']) ) {
+                $search = $_POST['search'];
+                // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+                $sql = "SELECT * FROM productos WHERE (id = '{$search}' OR nombre LIKE '%{$search}%' OR marca LIKE '%{$search}%' OR detalles LIKE '%{$search}%') AND eliminado = 0";
+                if ( $result = $conexion->query($sql) ) {
+                    // SE OBTIENEN LOS RESULTADOS
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+                    if(!is_null($rows)) {
+                        // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                        foreach($rows as $num => $row) {
+                            foreach($row as $key => $value) {
+                                $data[$num][$key] = utf8_encode($value);
+                            }
+                        }
+                    }
+                    $result->free();
+                } else {
+                    die('Query Error: '.mysqli_error($conexion));
+                }
+                $conexion->close();
+            } 
+        }
+
+        public function single(){
+            // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+            $data = array();
+            // SE VERIFICA HABER RECIBIDO EL ID
+            if( isset($_POST['id']) ) {
+                $id = $_POST['id'];
+                $sql = "SELECT * from productos WHERE id = {$id}";
+                if ( $result = $conexion->query($sql) ) {
+                    // SE OBTIENEN LOS RESULTADOS
+                    $rows = $result->fetch_array(MYSQLI_ASSOC);
+
+                    if(!is_null($rows)) {
+                        // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                        foreach($rows as $key => $value) {
+                            $data[$key] = utf8_encode($value);
+                        }
+                    }
+                    $result->free();
+                } else {
+                    die('Query Error: '.mysqli_error($conexion));
+                }
+                $conexion->close();
+            } 
+        }
+
+        public function singleByName(){
+            // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+            $data = array();
+            // SE VERIFICA HABER RECIBIDO EL ID
+            if( isset($_POST['nombre']) ) {
+                $nombre = $_POST['nombre'];
+                $sql = "SELECT * from productos WHERE nombre = {$nombre}";
+                if ( $result = $conexion->query($sql) ) {
+                    // SE OBTIENEN LOS RESULTADOS
+                    $rows = $result->fetch_array(MYSQLI_ASSOC);
+                    if(!is_null($rows)) {
+                        // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                        foreach($rows as $key => $value) {
+                            $data[$key] = utf8_encode($value);
+                        }
+                    }
+                    $result->free();
+                } else {
+                    die('Query Error: '.mysqli_error($conexion));
+                }
+                $conexion->close();
+            } 
+        }
+
+        
+
     }
 
 ?>
